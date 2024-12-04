6c786f1ac52582477a3dc76055d0d61ff6d43b12
KovacsZoltanDaniel <kovacs.zoltan@student.ms.sapientia.ro>
2024-12-04 20:00:19 +0200

    yaml config parser

--- src/yaml_config_parser.py
+++ src/yaml_config_parser.py
@@ -1,24 +1,30 @@
 import yaml
 
-# Input fájl beolvasása (input.txt)
-with open('input.txt', 'r') as file:
-    # A fájl tartalmának beolvasása
-    yaml_content = file.read()
 
-    # YAML tartalom betöltése a python dict-be
-    config = yaml.safe_load(yaml_content)
+with open('config.yaml', 'r') as file:
 
-# Rekurzív függvény az adatok formázott kiírásához
+    config = yaml.safe_load(file)
+
+
 def write_config(data, file, indent=0):
     for key, value in data.items():
         if isinstance(value, dict):
             file.write(" " * indent + f"{key}:\n")
             write_config(value, file, indent + 2)
+        elif isinstance(value, list):
+            file.write(" " * indent + f"{key}:\n")
+            for item in value:
+                if isinstance(item, dict):
+                    file.write(" " * (indent + 2) + "-\n")
+                    write_config(item, file, indent + 4)
+                else:
+                    file.write(" " * (indent + 2) + f"- {item}\n")
         else:
             file.write(" " * indent + f"{key}: {value}\n")
 
-# Kimeneti fájlba írás
+
 with open('output.txt', 'w') as output_file:
     write_config(config, output_file)
 
 print("A konfigurációs adatok kiírása befejeződött az output.txt fájlba.")
+
